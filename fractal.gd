23b09f46637703d37051a9fd24762194c2c577ae
kirkatia98 <kirkatia98@gmail.com>
2024-12-30 19:05:11 +0100

    zoom and pan (correct)

--- fractal.gd
+++ fractal.gd
@@ -1,35 +1,42 @@
 @tool
 class_name Fractal extends ColorRect
 
-enum Type {MANDLEBROT, JULIA}
+
+@export var pan_speed : float = 0.1
+@export var zoom_speed : float = 0.1
 
 
-@export var zoom_offset : Vector2 = Vector2(2, 2):
+@export var rect_position : Vector2 = Vector2(2, 2):
 	set(val):
-		zoom_offset = val
-		(material as ShaderMaterial).set_shader_parameter("offset", zoom_offset)
+		rect_position = val
+		(material as ShaderMaterial).set_shader_parameter("rect_position", rect_position)
 
 
-@export var zoom_scale : Vector2 = Vector2(4, 4):
+@export var rect_size : Vector2 = Vector2(4, 4):
 	set(val):
-		zoom_scale = val
-		(material as ShaderMaterial).set_shader_parameter("scale", zoom_scale)
-
-
-
+		rect_size = val
+		(material as ShaderMaterial).set_shader_parameter("rect_size", rect_size)
 
 @onready var shader_material : ShaderMaterial = material as ShaderMaterial
-
-
-
 
 
 # override gui input instead of input so that zoom input only accepted inside the borders
 func _gui_input(event):
 	if event.is_action_pressed("zoom_in", true):
+		var old_center = 0.5 * rect_size + rect_position
+		var new_size = rect_size * 0.9
+		var new_center = 0.5 * new_size + rect_position
+		rect_position += new_center - old_center
+		rect_size = new_size
 
-		zoom_scale += Vector2(0.1, 0.1)
 
 	if event.is_action_pressed("zoom_out", true):
+		var old_center = 0.5 * rect_size + rect_position
+		var new_size = rect_size / 0.9
+		var new_center = 0.5 * new_size + rect_position
+		rect_position += new_center - old_center
+		rect_size = new_size
 
-		zoom_scale -= Vector2(0.1, 0.1)
+func _unhandled_input(_event):
+	var input_vector : Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	rect_position -= 0.1 * rect_size * input_vector
