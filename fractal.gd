4a28a01a7790242bf94bd21ade5cba03d255ffd3
kirkatia98 <kirkatia98@gmail.com>
2024-12-30 16:20:43 +0100

    basic (wrong) zooming, transform function and paramaters set

--- fractal.gd
+++ fractal.gd
@@ -1,28 +1,35 @@
-class_name Fractal extends SubViewportContainer
+@tool
+class_name Fractal extends ColorRect
 
-@export var zoom_center : Vector2 = Vector2(2.0, 2.0)
-@export var zoom_amount : Vector2 = Vector2(4.0, 4.0)
+enum Type {MANDLEBROT, JULIA}
 
-@onready var color_rect : ColorRect = $SubViewport/ColorRect
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	#color_rect.material.set_shader_parameter("zoom_center", Vector2(3, 2))
-	pass
+@export var zoom_offset : Vector2 = Vector2(2, 2):
+	set(val):
+		zoom_offset = val
+		(material as ShaderMaterial).set_shader_parameter("offset", zoom_offset)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
+
+@export var zoom_scale : Vector2 = Vector2(4, 4):
+	set(val):
+		zoom_scale = val
+		(material as ShaderMaterial).set_shader_parameter("scale", zoom_scale)
+
+
+
+
+@onready var shader_material : ShaderMaterial = material as ShaderMaterial
+
+
+
 
 
 # override gui input instead of input so that zoom input only accepted inside the borders
 func _gui_input(event):
 	if event.is_action_pressed("zoom_in", true):
-		print(name + " zoom in")
 
-		zoom_amount += Vector2(0.1, 0.1)
+		zoom_scale += Vector2(0.1, 0.1)
 
 	if event.is_action_pressed("zoom_out", true):
-		print(name + " zoom out")
 
-		zoom_amount += Vector2(0.1, 0.1)
+		zoom_scale -= Vector2(0.1, 0.1)
