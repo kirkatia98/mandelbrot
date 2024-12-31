d3e85ecd7b576b95a59fcb5ac735215b671c93d7
kirkatia98 <kirkatia98@gmail.com>
2024-12-31 12:49:07 +0100

    Simplified zoom code (finally lol)

--- fractal.gd
+++ fractal.gd
@@ -9,7 +9,7 @@ class_name Fractal extends ColorRect
 @export var pan_speed : float = 0.1
 @export var zoom_speed : float = 0.1
 
-@export var rect_position : Vector2 = Vector2(2, 2):
+@export var rect_position : Vector2 = Vector2(-2, -2):
 	set(val):
 		rect_position = val
 		shader_material.set_shader_parameter("rect_position", rect_position)
@@ -47,20 +47,17 @@ func update_coords():
 
 # override gui input instead of input so that zoom input only accepted inside the borders
 func _gui_input(event):
+	var old_center = 0.5 * rect_size + rect_position
+
 	if event.is_action_pressed("zoom_in", true):
-		var old_center = 0.5 * rect_size + rect_position
-		var new_size = rect_size * 0.9
-		var new_center = 0.5 * new_size + rect_position
-		rect_position += new_center - old_center
-		rect_size = new_size
+		rect_size = rect_size * 0.9
 
 
 	if event.is_action_pressed("zoom_out", true):
-		var old_center = 0.5 * rect_size + rect_position
-		var new_size = rect_size / 0.9
-		var new_center = 0.5 * new_size + rect_position
-		rect_position += new_center - old_center
-		rect_size = new_size
+		rect_size = rect_size / 0.9
+
+	rect_position = old_center - 0.5 * rect_size
+
 
 func _unhandled_input(_event):
 	var input_vector : Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
